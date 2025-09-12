653afaa34eaf73d9428a45d0b571fd06fae4a328
saikumar <saiponnana100@gmail.com>
2025-09-12 10:56:27 +0530

    Initial commit

---
+++ BACKEND/controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require("../models/user");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "Error registering user", error });
+  }
+};
+
+module.exports = { registerUser };
